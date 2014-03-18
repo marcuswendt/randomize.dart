60938b277c60efdba567b6fa0cc3d796ebfd71df
Marcus Wendt <marcus@field.io>
2014-03-18 16:10:13 +0000

    Switched from class methods over to static function format.

--- lib/randomize.dart
+++ lib/randomize.dart
@@ -1,24 +1,36 @@
-import 'dart:math';
-
 /**
- * Utility class to help with random number generation of various sorts
+ * Utility methods to help with random number generation of various sorts.
  */
-class Randomize
-{
-  int _seed;
-  Random _rng;
+import 'dart:math';
+import 'package:vector_math/vector_math.dart';
+
+
+// static global versions
+Random _rng = new Random(0);
+
+seed(int seed) => _rng = new Random();
+
+/** returns a random normal between -scale, scale */
+double randn([double scale=1.0]) => (_rng.nextDouble() * 2.0 - 1.0) * scale;
+
+/** returns a random double number between min, max */
+double randd([double min=0.0, double max=1.0]) => _rng.nextDouble() * (max - min) + min;
+
+/** returns a random integer number between min, max */
+int randi([int min=0, int max=1]) => _rng.nextInt(max - min) + min;
+
+Vector3 randv([double min=0.0, double max=1.0, Vector3 value=null]) {
+  if(value == null)
+    value = new Vector3.zero();
   
-  Randomize(this._seed) {
-    _rng = new Random(_seed);  
-  }
+  double range = max - min;
   
-  double normal([double scale=1.0]) => (_rng.nextDouble() * 2.0 - 1.0) * scale;
-  
-  double randd([double min=0.0, double max=1.0]) => _rng.nextDouble() * (max - min) + min;  
-  
-  int randi([int min=0, int max=1]) => _rng.nextInt(max - min) + min;  
-    
-  bool flipCoin([double chance = 0.5]) => _rng.nextDouble() < chance;
-  
-  int get seed => _seed;
+  return value.setValues(
+      _rng.nextDouble() * range + min,
+      _rng.nextDouble() * range + min,
+      _rng.nextDouble() * range + min
+  );
 }
+
+/** returns true/ false based on a given chance */
+bool flipCoin([double chance = 0.5]) => _rng.nextDouble() < chance;
